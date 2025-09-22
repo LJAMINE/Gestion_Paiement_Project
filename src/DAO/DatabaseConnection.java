71e77346ad2eeac6806aa4d07d16a2de688a50cb
LJAMINE <aminelmgrmj@gmail.com>
2025-09-22 23:27:27 +0100

    adding the connection fonctionnality using Singleton in DAO

--- src/DAO/DatabaseConnection.java
+++ src/DAO/DatabaseConnection.java
@@ -1,4 +1,29 @@
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private Connection connection;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/testnavette";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    private DatabaseConnection() throws SQLException {
+        connection = DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static DatabaseConnection getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import DAO.DatabaseConnection;
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        try {
+            Connection conn = DatabaseConnection.getInstance().getConnection();
+            if (conn != null) {
+                System.out.println("Connection successful!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
